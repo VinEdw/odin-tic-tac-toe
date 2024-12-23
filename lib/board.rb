204b9cb9ec6a027f682c13a330ef7eaacbfcc152
Vincent Edwards <mister.man489@gmail.com>
2024-12-22 22:20:17 -0800

    Define a method to get a diagonal

--- lib/board.rb
+++ lib/board.rb
@@ -13,4 +13,22 @@ class Board
   def column(index)
     @grid.map { |row| row[index] }
   end
+
+  def diagonal(index)
+    arr = []
+
+    if index == 0
+      (0...size).each do |index|
+        arr << @grid.dig(index, index)
+      end
+    elsif index == 1
+      (0...size).each do |index|
+        arr << @grid.dig(size - index - 1, index)
+      end
+    else
+      return nil
+    end
+
+    arr
+  end
 end
